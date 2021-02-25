06ec780cc408ff97befe50d8ef23f28154fb88b0
Omer <orubin65@gmail.com>
2021-02-26 01:43:45 +0200

    Rotated percentage text

--- js/countdown.js
+++ js/countdown.js
@@ -6,141 +6,91 @@ const startDate = new Date("October 23, 2019 0:00:00").getTime();
 const endDate = new Date("June 22, 2022 0:00:00").getTime();
 
 
-
-
 particlesJS('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 160,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#ffffff"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 200,
-        "color": "#ffffff",
-        "opacity": 0.2,
-        "width": 0.7
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.5,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": false,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#000000",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
+	{
+		"particles": {
+			"number": {
+				"value": 160,
+				"density": {
+					"enable": true,
+					"value_area": 800
+				}
+			},
+			"color": {
+				"value": "#ffffff"
+			},
+			"shape": {
+				"type": "circle",
+				"stroke": {
+					"width": 0,
+					"color": "#ffffff"
+				},
+				"polygon": {
+					"nb_sides": 5
+				},
+				"image": {
+					"src": "img/github.svg",
+					"width": 100,
+					"height": 100
+				}
+			},
+			"opacity": {
+				"value": 0.5,
+				"random": true,
+				"anim": {
+					"enable": false,
+					"speed": 1,
+					"opacity_min": 0.1,
+					"sync": false
+				}
+			},
+			"size": {
+				"value": 3,
+				"random": true,
+				"anim": {
+					"enable": false,
+					"speed": 40,
+					"size_min": 0.1,
+					"sync": false
+				}
+			},
+			"line_linked": {
+				"enable": true,
+				"distance": 50,
+				"color": "#ffffff",
+				"opacity": 0.1,
+				"width": 0.3
+			},
+			"move": {
+				"enable": true,
+				"speed": 0.5,
+				"direction": "none",
+				"random": false,
+				"straight": false,
+				"out_mode": "out",
+				"attract": {
+					"enable": false,
+					"rotateX": 600,
+					"rotateY": 1200
+				}
+			}
+		},
+	}
 );
 
 
 setInterval(() => {
-  var currentDate = new Date().getTime();
-  
+	var currentDate = new Date().getTime();
+	
 	var distance = endDate - currentDate;
 	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor(distance % (1000 * 60) / 1000);
-  countdown.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
-    
+	countdown.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+		
 	var percentDone = (currentDate - startDate) / (endDate - startDate) * 100;
-	progressBar.style.strokeDasharray = percentDone.toFixed(2) + '100';
-	percentProgress.innerHTML = percentDone.toFixed(2) + '%';
+	progressBar.style.strokeDasharray = percentDone.toFixed(1) + '100';
+	percentProgress.innerHTML = percentDone.toFixed(1) + '%';
+	percentProgress.setAttribute('transform', 'translate(16, 16) rotate(90)');
 }, 1000);
